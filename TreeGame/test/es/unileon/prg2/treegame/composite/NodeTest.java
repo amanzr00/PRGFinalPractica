f0021f3c0ef3e0f7e34524a76f4f872404bd5a67
amanzr00 <amanzr00@estudiantes.unileon.es>
2014-01-30 20:32:46 +0100

    Changes in tests
    Cambios en los test nodetest y nodehandlertest

--- TreeGame/test/es/unileon/prg2/treegame/composite/NodeTest.java
+++ TreeGame/test/es/unileon/prg2/treegame/composite/NodeTest.java
@@ -10,21 +10,5 @@ import es.unileon.prg2.treegame.handler.NodeHandler;
  * @version 1.0
  */
 public abstract class NodeTest {
-	private NodeHandler number1, number2, number3;
-	int id, id2;
-	@Before
-	public void setUp() throws Exception {
-		id = 1;
-		id2 = 2;
-		number1 = new NodeHandler(id);
-		number2 = new NodeHandler(id);
-		number3 = new NodeHandler(id2);
-	}
-
-	@Test
-	public void testcompareTo() {
-		equals(this.number1.toString().compareTo(number2.toString()));
-		equals(this.number1.toString().compareTo(number3.toString()));
-		
-	}
+	
 }
--- TreeGame/test/es/unileon/prg2/treegame/handler/NodeHandlerTest.java
+++ TreeGame/test/es/unileon/prg2/treegame/handler/NodeHandlerTest.java
@@ -1,5 +1,8 @@
 package es.unileon.prg2.treegame.handler;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * @author Alberto Manzano.
  * @author Iván Montes.
@@ -7,4 +10,21 @@ package es.unileon.prg2.treegame.handler;
  * @version 1.0
  */
 public class NodeHandlerTest {
+	private NodeHandler number1, number2, number3;
+	int id, id2;
+	@Before
+	public void setUp() throws Exception {
+		id = 1;
+		id2 = 2;
+		number1 = new NodeHandler(id);
+		number2 = new NodeHandler(id);
+		number3 = new NodeHandler(id2);
+	}
+
+	@Test
+	public void testcompareTo() {
+		equals(this.number1.toString().compareTo(number2.toString()));
+		equals(this.number1.toString().compareTo(number3.toString()));
+		
+	}
 }
